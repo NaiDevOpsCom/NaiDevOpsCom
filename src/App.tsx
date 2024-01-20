d132f6153d189ac2d3998f545b46a2fe941aa243
McRonaah <ronaldcheruiyot342@gmail.com>
2024-01-20 12:46:44 +0300

    :art: updated routing in main file :memo:

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import NavBar from "./components/HomePage/NavBar";
+import HomePage from "./components/HomePage/HomePage";
 
 function App() {
   
 
   return (
-    <div>
-      Hello Mongina
-    </div>
+    <BrowserRouter>
+        <NavBar />
+        <Routes>
+        <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
   )
 }
 
