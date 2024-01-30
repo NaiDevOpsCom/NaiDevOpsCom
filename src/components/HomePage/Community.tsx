797b1b64a45068155d9497be244e36291c8dbabd
McRonaah <ronaldcheruiyot342@gmail.com>
2024-01-30 11:13:43 +0300

    :pushpin: :fixed: all home pages routings :art:

---
+++ src/components/HomePage/Community.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Community = () => {
+  return (
+    <div>
+      <h2>Community feedback coming soon.</h2>
+    </div>
+  )
+}
+
+export default Community;
---
+++ src/components/HomePage/DevOpsEvents.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DevOpsEvents = () => {
+  return (
+    <div>
+      <h3>Devops events section coming soon.</h3>
+    </div>
+  )
+}
+
+export default DevOpsEvents;
